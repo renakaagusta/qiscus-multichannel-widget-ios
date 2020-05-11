ec970df67cd01e254ebb63661ac71c98a1830792
asharijuang <asharijuang@me.com>
2020-05-11 13:33:59 +0700

    unknown

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        MultichannelWidget.setup(appID: "hap-vq8ahvrbmhiyypcmg")
+        MultichannelWidget.setup(appID: "")
         return true
     }
 
