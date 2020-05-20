855d64c1f77c9f8e436ae5365fa7296e0b979f20
amsibsam <bisma@qiscus.net>
2020-05-20 19:51:39 +0700

    remove unused color predefined value

--- MultichannelWidget/Config/ColorConfiguration.swift
+++ MultichannelWidget/Config/ColorConfiguration.swift
@@ -9,11 +9,6 @@
 import UIKit
 
 class ColorConfiguration: NSObject {
-    /// Your cancel button color, using UIColor class, Default value : UIColor(red: 223/255.0, green: 223/255.0, blue: 223/255.0, alpha:1.0)
-    static var cancelButtonColor = UIColor(red: 223/255.0, green: 223/255.0, blue: 223/255.0, alpha:1.0)
-    
-    /// Your alert text color, using UIColor class, Default value : UIColor(red: 155/255.0, green: 155/255.0, blue: 155/255.0, alpha:1.0)
-    static var alertTextColor = UIColor(red: 155/255.0, green: 155/255.0, blue: 155/255.0, alpha:1.0)
     
     /// Your left bubble color, using UIColor class, Default value : UIColor(red: 0/255.0, green: 187/255.0, blue: 150/255.0, alpha: 1.0)
     static var leftBubbleColor = UIColor(red: 0.957, green: 0.957, blue: 0.957, alpha: 1)
@@ -56,54 +51,11 @@ class ColorConfiguration: NSObject {
     /// Your link color of right bubble chat, using UIColor class, Default value : UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
     static var rightBubbleLinkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
     
-    /// Your background color of lock view, using UIColor class, Default value : UIColor(red: 255.0/255.0, green: 87/255.0, blue: 34/255.0, alpha: 1)
-    static var lockViewBgColor = UIColor(red: 255.0/255.0, green: 87/255.0, blue: 34/255.0, alpha: 1)
-    
-    /// Your tint color of lock view, using UIColor class, Default value : UIColor.blackColor()
-    static var lockViewTintColor = UIColor.black
-    
-    /// Welcome image color, using UIColor class, Default value: UIColor(red: 18/255.0, green: 180/255.0, blue: 147/255.0, alpha: 1)
-    static var welcomeIconColor = UIColor(red: 18/255.0, green: 180/255.0, blue: 147/255.0, alpha: 1)
-    
     static var topColor = UIColor(red: 0, green: 0.443, blue: 0.733, alpha: 1)
     
     static var bottomColor = UIColor(red: 23/255.0, green: 177/255.0, blue: 149/255.0, alpha: 1)
     
     static var tintColor = UIColor.white
-    
-    /// postback button color, using UIColor class, Default value: UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
-    static var postBackButtonColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
-    
-    static var avatarBackgroundColor:[UIColor] = [
-        UIColor(red: 1, green: 23/255, blue: 68/255, alpha: 1),
-        UIColor(red: 61/255, green: 90/255, blue: 254/255, alpha: 1),
-        UIColor(red: 198/255, green: 1, blue: 0, alpha: 1),
-        UIColor(red: 29/255, green: 233/255, blue: 182/255, alpha: 1),
-        UIColor(red: 1, green: 145/255, blue: 0, alpha: 1),
-        UIColor(red: 0, green: 176/255, blue: 1, alpha: 1),
-        UIColor(red: 118/255, green: 1, blue: 3/255, alpha: 1),
-        UIColor(red: 1, green: 61/255, blue: 0, alpha: 1),
-        UIColor(red: 101/255, green: 31/255, blue: 1, alpha: 1),
-        UIColor(red: 1, green: 196/255, blue: 0, alpha: 1),
-        UIColor(red: 41/255, green: 121/255, blue: 1, alpha: 1),
-        UIColor(red: 1, green: 234/255, blue: 0, alpha: 1),
-        UIColor(red: 213/255, green: 0, blue: 249/255, alpha: 1),
-        UIColor(red: 0, green: 230/255, blue: 118/255, alpha: 1),
-        UIColor(red: 245/255, green: 0, blue: 87/255, alpha: 1),
-        UIColor(red: 0, green: 229/255, blue: 1, alpha: 1)
-        ]
-    
-    static var randomColorLabelName:[UIColor] = [
-        UIColor(red: 131/255, green: 147/255, blue: 202/255, alpha: 1),
-        UIColor(red: 53/255, green: 205/255, blue: 150/255, alpha: 1),
-        UIColor(red: 186/255, green: 51/255, blue: 220/255, alpha: 1),
-        UIColor(red: 2/255, green: 157/255, blue: 0/255, alpha: 1),
-        UIColor(red: 253/255, green: 133/255, blue: 212/255, alpha: 1),
-        UIColor(red: 139/255, green: 122/255, blue: 221/255, alpha: 1),
-        UIColor(red: 223/255, green: 182/255, blue: 16/255, alpha: 1),
-        UIColor(red: 176/255, green: 70/255, blue: 50, alpha: 1),
-        UIColor(red: 41/255, green: 169/255, blue: 42/255, alpha: 1)
-    ]
     
     static var baseColor:UIColor{
         get{
