8d30569903922527aaf369263f6d94962cbfa5fd
asharijuang <asharijuang@me.com>
2020-06-15 07:22:52 +0700

    fix comma

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "multichannelwidget",
     platforms: [
         .iOS(.v8)
-    ]
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: https://url/of/another/package/named/Utility, from: "1.0.0"),
         // .package(path: "MultichannelWidget")
     ],
     targets: [
