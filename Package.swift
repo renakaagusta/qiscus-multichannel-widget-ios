65c85567054ed2ff0f6963a8327cea219cb46441
asharijuang <asharijuang@me.com>
2020-06-15 07:31:59 +0700

    update name

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "multichannelwidget",
-            targets: ["multichannelwidget"]),
+            name: "MultichannelWidget",
+            targets: ["MultichannelWidget"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "multichannelwidget",
+            name: "MultichannelWidget",
             dependencies: []),
         .testTarget(
-            name: "multichannelwidgetTests",
-            dependencies: ["multichannelwidget"]),
+            name: "MultichannelWidgetTests",
+            dependencies: ["MultichannelWidget"]),
     ]
 )
