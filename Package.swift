9216d91ba89b7483b30c547324dd00fcaf0c1cf2
asharijuang <asharijuang@me.com>
2020-06-18 21:05:23 +0700

    add core api

--- Package.swift
+++ Package.swift
@@ -19,14 +19,15 @@ let package = Package(
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+        .package(url: "https://github.com/Qiscus-Integration/QiscusCoreApi.git", branch: "spm")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MultichannelWidget",
-            dependencies: ["SwiftyJSON", "Alamofire", "AlamofireImage", "SDWebImage"]),
+            dependencies: ["SwiftyJSON", "Alamofire", "AlamofireImage", "SDWebImage", "QiscusCoreApi"]),
         .testTarget(
             name: "MultichannelWidgetTests",
             dependencies: ["MultichannelWidget"]),
--- swift package manager/swift package manager/ViewController.swift
+++ swift package manager/swift package manager/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import MultichannelWidget
+import QiscusCoreAPI
 
 class ViewController: UIViewController {
 
