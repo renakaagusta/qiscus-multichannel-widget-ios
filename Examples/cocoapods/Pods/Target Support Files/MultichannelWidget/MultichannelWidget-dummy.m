d33c4269945c9dc8ad5d6298486d947a362d2f4a
asharijuang <asharijuang@me.com>
2020-06-28 23:48:51 +0700

    remove unused file

--- Examples/cocoapods/Pods/Target Support Files/MultichannelWidget/MultichannelWidget-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MultichannelWidget : NSObject
-@end
-@implementation PodsDummy_MultichannelWidget
-@end
--- Examples/cocoapods/Pods/Target Support Files/QiscusCoreAPI/QiscusCoreAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QiscusCoreAPI : NSObject
-@end
-@implementation PodsDummy_QiscusCoreAPI
-@end
